9458dff429e90f399e5ea82dc398941d840d3e6e
Lawrence Hunt <lawrence.hunt@hotmail.com>
2018-07-09 22:08:13 +0100

    got to video 7

---
+++ starter-files/controllers/storeController.js
@@ -0,0 +1,3 @@
+exports.storeController = (req, res) => {
+  // res.json({name: 'Lawrence', age: '28', cool: false});
+  res.render('hello'))
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController')
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get('/', storeController)
+
+router.get('/reverse/:name', (req, res) => {
+  // res.json({name: 'Lawrence', age: '28', cool: false});
+  console.log(req)
+  res.send([...req.params.name].reverse().join(''))
 });
+
+
 
 module.exports = router;
